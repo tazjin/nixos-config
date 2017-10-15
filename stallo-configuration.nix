6845e2efd4d94441d9c6af1614bc4058fca51aa7
Vincent Ambo <tazjin@gmail.com>
2017-10-15 20:02:24 +0200

    fix(stallo): Post-installatio wifi, gfx, luks fixes

--- stallo-configuration.nix
+++ stallo-configuration.nix
@@ -3,10 +3,15 @@
 { config, ...}:
 
 {
+  boot.initrd.luks.devices.stallo-luks.device = "/dev/disk/by-uuid/b484cf1e-a27b-4785-8bd6-fa85a004b073";
+
+  # Use proprietary nvidia driver
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   networking.hostName = "stallo";
   networking.wireless.enable = true;
   networking.wireless.networks = {
-    "How do I computer fast?" = {
+    "How do I computer?" = {
       # Welcome to roast club!
       psk = "washyourface";
     };
