b24b4b54d45074e73effd8d832b6fd611d534b32
Vincent Ambo <tazjin@gmail.com>
2017-10-16 13:47:10 +0200

    refactor(adho): Replace connman with nix-based wifi config
    Now that WPA2 is broken wifi passwords aren't secret anymore,
    anyways ;-)

--- adho-configuration.nix
+++ adho-configuration.nix
@@ -4,8 +4,26 @@
 {
   boot.initrd.luks.devices.adho.device = "/dev/disk/by-uuid/722006b0-9654-4ea1-8703-e0cf9ac1905e";
   networking.hostName = "adho";
-  networking.connman.enable = true;
   services.xserver.libinput.enable = true;
   services.xserver.videoDrivers = [ "intel" ];
   programs.light.enable = true;
+
+  networking.wireless.enable = true;
+  networking.wireless.networks = {
+    # Welcome to roast club!
+    "How do I computer?" = {
+      psk = "washyourface";
+    };
+
+    # Did someone say wifi credentials are secret?
+    # http://bit.ly/2gI43QP
+    "Amesto-mobile" = {
+      psk = "ostemAt1";
+    };
+
+    # Public places in Oslo:
+    "Abelone" = {
+      psk = "speakeasy";
+    };
+  };
 }
